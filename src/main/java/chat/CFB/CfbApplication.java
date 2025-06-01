2e00c7bcf4100cd731931baebc48538ce7c90f86
ManuelLazaro <gorselazarorosas@gmail.com>
2025-06-01 14:24:28 -0500

    cry

---
+++ src/main/java/chat/CFB/CfbApplication.java
@@ -0,0 +1,13 @@
+package chat.CFB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chat/CFB/CfbApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.CFB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
